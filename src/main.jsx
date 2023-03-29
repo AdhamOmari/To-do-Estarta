40a3b5fac642dfa30e081777d5cafe2cd823862b
Adham Omari <adhamomarics@gmail.com>
2023-03-29 19:58:20 +0300

    solve

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { Provider } from 'redux'
 import App from './App'
 import './index.css'
 import { store } from './redux/store'
